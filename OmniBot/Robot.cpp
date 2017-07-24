5809ed363cf19da3341394d6580ab1619ff58a0d
Theodore-Regina <veilleuxtrinh.theodore@cra.education>
2017-07-24 19:28:54 -0400

    Boucle d'opperation de base + réaction à differentes problematiques (Chaleur, Bris moteur, etc...)

--- OmniBot/Robot.cpp
+++ OmniBot/Robot.cpp
@@ -22,12 +22,14 @@ Robot::~Robot()
 void Robot::Run()
 {
 	static int STATE_MACHINE = GET_GOAL;
+	static int PREV_STATE_MACHINE = STATE_MACHINE;
 
 	switch(STATE_MACHINE)
 	{
 		case GET_GOAL:
 		{
 			m_dir = this->GetComm();
+			PREV_STATE_MACHINE = STATE_MACHINE;
 			STATE_MACHINE = SEND_DATA;
 			
 			break;
@@ -37,6 +39,7 @@ void Robot::Run()
 		{
 			this->SendArduino("AD=", m_dir.Angle);
 			this->SendArduino("VD=", m_dir.Val);
+			PREV_STATE_MACHINE = STATE_MACHINE;
 			STATE_MACHINE = RETRIEVE_DATA;
 
 			break;
@@ -44,32 +47,81 @@ void Robot::Run()
 
 		case RETRIEVE_DATA:
 		{
-			
+			State TempState();
+
+			m_state = TempState;
+
+			PREV_STATE_MACHINE = STATE_MACHINE;
+			STATE_MACHINE = DATA_ANALITIC;
+			break;
 		}
 
 		case DATA_ANALITIC:
 		{
-		
+			static Step = 0;
+			
+			if(m_state.IsOnFire && Step == 0)
+			{
+				Step++;
+				PREV_STATE_MACHINE = STATE_MACHINE;
+				STATE_MACHINE = IS_ON_FIRE;
+				break;
+			}
+			
+			if((!m_state.Mot1 || !m_state.Mot2 || !m_state.Mot3) && Step == 1)
+			{
+				Step++;
+				PREV_STATE_MACHINE = STATE_MACHINE;
+				STATE_MACHINE = MOTOR_DOWN;
+				break;
+			}
+
+			if(m_state.IsLowBat && Step == 2)
+			{
+				Step++;
+				PREV_STATE_MACHINE = STATE_MACHINE;
+				STATE_MACHINE = LOW_BATTERY;
+				break;
+			}
+
+			PREV_STATE_MACHINE = DATA_ANALITIC;
+			STATE_MACHINE = GET_GOAL;
+			Step = 0;
+			break;
 		}
 
 		case ON_FIRE:
 		{
-		
-		}
+			SendArduino("KFN", 0);
 
-		case ON_BACK:
-		{
-		
+			if(this->CanShutdown)
+			{
+				SendArduino("STD", 1); //Shutdown as quick as possible
+			}
+
+			else 
+			{
+				PREV_STATE_MACHINE = STATE_MACHINE;
+				STATE_MACHINE = DATA_ANALITIC;
+			}
+			break;
 		}
 
 		case MOTOR_DOWN:
 		{
-		
-		}
+			if(!this->CanShutDown)
+			{
+				PREV_STATE_MACHINE = STATE_MACHINE;
+				STATE_MACHINE = DATA_ANALITIC;
+				SendArduino("DCC", 0); //DriveControChanges ... 
+			}
 
-		case NOT_ON_GROUND:
-		{
-		
+			else
+			{
+				SendArduino("STD", 0); // Shutdown 
+			}
+
+			break;
 		}
 	}
 	
